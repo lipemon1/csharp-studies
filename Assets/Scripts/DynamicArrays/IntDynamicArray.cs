7fdb982a9bd132eb0d51ef727438300a7e2b66fc
Ramon Ferreira <ramon_felipe@outlook.com>
2020-07-13 21:36:27 -0300

    First class to handle Integers dynamics arrays
    Created a class that will handle integers arrays dynamically

---
+++ Assets/Scripts/DynamicArrays/IntDynamicArray.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using UnityEngine;
+
+namespace DynamicArrays
+{
+    public abstract class IntDynamicArray
+    {
+        private const int ExpandMultiplyFactor = 2;
+        protected int[] items;
+        protected int count;
+
+        #region Constructor
+
+        protected IntDynamicArray()
+        {
+            items = new int[4];
+            count = 0;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public abstract void Add(int item);
+        public abstract bool Remove(int item);
+        public abstract int IndexOf(int item);
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < count; i++)
+            {
+                builder.Append(items[i]);
+                if (i < count - 1)
+                {
+                    builder.Append(",");
+                }
+            }
+
+            return builder.ToString();
+        }            
+
+        #endregion
+
+        #region Protected Methods
+
+        protected void Expand()
+        {
+            var newItems = new int[items.Length * ExpandMultiplyFactor];
+
+            for (var i = 0; i < items.Length; i++)
+            {
+                newItems[i] = items[i];
+            }
+
+            items = newItems;
+        }
+
+        #endregion
+    }
+}
