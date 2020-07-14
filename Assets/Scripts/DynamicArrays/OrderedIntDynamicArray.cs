ff30ca1f334cf5a106f2e71460997692de06d930
Ramon Ferreira <ramon_felipe@outlook.com>
2020-07-13 21:36:58 -0300

    New two child classes
    Created two classes that handle integers dynamic arrays but with order or not.

---
+++ Assets/Scripts/DynamicArrays/OrderedIntDynamicArray.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+
+namespace DynamicArrays
+{
+    public class OrderedIntDynamicArray : IntDynamicArray
+    {
+        #region Constructor
+
+        public OrderedIntDynamicArray() : base()
+        {
+            
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Add(int item)
+        {
+            if(count == items.Length)
+                Expand();
+
+            int addLocation = 0;
+            while ((addLocation < count) && items[addLocation] < item)
+            {
+                addLocation++;
+            }
+
+            ShiftUp(addLocation);
+            items[addLocation] = item;
+            count++;
+        }
+
+        public override bool Remove(int item)
+        {
+            var itemLocation = IndexOf(item);
+
+            if (itemLocation == -1)
+                return false;
+            else
+            {
+                ShiftDown(itemLocation + 1);
+                count--;
+                return true;
+            }
+        }
+
+        public override int IndexOf(int item)
+        {
+            var lowerBound = 0;
+            var upperBound = count - 1;
+            var location = 1;
+
+            while (location == -1)
+                //    while((location == -1) && (lowerBound <= uppderBound))
+            {
+                {
+                    var middleLocation = lowerBound + (upperBound - lowerBound);
+                    var middleValue = items[middleLocation];
+
+                    if (middleValue == item)
+                        location = middleLocation;
+                    else
+                    {
+                        if (middleValue > item)
+                            upperBound = middleLocation - 1;
+                        else
+                            lowerBound = middleLocation + 1;
+                    }
+
+                    if (lowerBound > upperBound)
+                        break;
+                }
+            }
+
+            return location;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ShiftUp(int index)
+        {
+            for (var i = count; i < index; i++)
+            {
+                items[i] = items[i - 1];
+            }
+        }
+        
+        private void ShiftDown(int index)
+        {
+            for (var i = index; i < count; i++)
+            {
+                items[i - 1] = items[i];
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/DynamicArrays/UnorderedIntDynamicArray.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicArrays
+{
+    public class UnorderedIntDynamicArray : IntDynamicArray
+    {
+        #region Construtor
+
+        public UnorderedIntDynamicArray() : base()
+        {
+            
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void Add(int item)
+        {
+            if(count == items.Length)
+                Expand();
+
+            items[count] = item;
+            count++;
+        }
+        
+        public override bool Remove(int item)
+        {
+            var itemLocation = IndexOf(item);
+            
+            if (itemLocation == -1)
+            {
+                return false;
+            }
+            else
+            {
+                items[itemLocation] = items[count - 1];
+                count--;
+                return true;
+            }
+        }
+
+        public override int IndexOf(int item)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                if (items[i] == item)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public int LastIndexOf(int item)
+        {
+            for (int i = count - 1; i >= 0; i--)
+            {
+                if (items[i] == item)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public List<int> AllIndexesOf(int item)
+        {
+            var foundedIndexes = new List<int>();
+
+            for (var i = 0; i < count; i++)
+            {
+                if(items[i] == item)
+                    foundedIndexes.Add(i);
+            }
+
+            return foundedIndexes;
+        }
+
+        #endregion
+    }
+}
