9ea968e7cb7dba5b35efc807e630ae577a4a36d3
Ramon Ferreira <ramon_felipe@outlook.com>
2020-07-27 23:36:16 -0300

    Linked Lists

---
+++ Assets/Scripts/LinkedLists/LinkedListNode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LinkedLists
+{
+    /// <summary>
+    /// A linked list node
+    /// </summary>
+    public class LinkedListNode<T>
+    {
+        #region Fields
+
+        T value;
+        LinkedListNode<T> next;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates new node with given value and next node
+        /// </summary>
+        /// <param name="value">value</param>
+        /// <param name="next">next node</param>
+        public LinkedListNode(T value, LinkedListNode<T> next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the node value
+        /// </summary>
+        /// <value>node value</value>
+        public T Value
+        {
+            get { return value; }
+        }
+
+        /// <summary>
+        /// Gets and sets the next node
+        /// </summary>
+        /// <value>next node</value>
+        public LinkedListNode<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        #endregion
+    }
+}
